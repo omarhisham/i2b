7f168f6fe18b0a7c9d8a20501fab929e59ac6fb6
JirayrB <jirayr.benohanian@gmail.com>
2015-03-12 14:48:08 +0200

    Removed Update button

--- Project/app/controllers/events_controller.rb
+++ Project/app/controllers/events_controller.rb
@@ -39,17 +39,17 @@ class EventsController < ApplicationController
 
   # PATCH/PUT /events/1
   # PATCH/PUT /events/1.json
-  def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+   # respond_to do |format|
+    #  if @event.update(event_params)
+     #   format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+      #  format.json { render :show, status: :ok, location: @event }
+      #else
+       # format.html { render :edit }
+        #format.json { render json: @event.errors, status: :unprocessable_entity }
+      #end
+    #end
+  #end 
 
   # DELETE /events/1
   # DELETE /events/1.json
