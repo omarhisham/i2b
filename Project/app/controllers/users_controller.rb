8c67d1229ee2b5cec63196eeb5b7038b49d6592d
Hussien Kamal <hussienelhady14@gmail.com>
2015-03-12 20:52:44 +0200

    updates

--- Project/app/controllers/users_controller.rb
+++ Project/app/controllers/users_controller.rb
@@ -15,6 +15,14 @@ class UsersController < ApplicationController
     render "new"
   end
 
+    def index
+    if params[:approved] == "false"
+      @users = User.find_all_by_approved(false)
+    else
+      @users = User.all
+    end
+  end
+
 
    private
 
--- Project/app/models/user.rb
+++ Project/app/models/user.rb
@@ -4,15 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-        #  def active_for_authentication? 
-    #super && approved? 
-  #end 
+        def active_for_authentication? 
+    super && approved? 
+  end 
 
-  #def inactive_message 
-   # if !approved? 
-    #  :not_approved 
-    #else 
-     # super # Use whatever other message 
-    #end 
-  #end
+  def inactive_message 
+    if !approved? 
+      :not_approved 
+    else 
+      super # Use whatever other message 
+    end 
+  end
 end
